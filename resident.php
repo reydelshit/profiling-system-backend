1cfdace640268f976356ac9583d754a5f7c61ded
Reydel Ocon <reydel321@gmail.com>
2023-11-28 18:20:39 +0800

    crud

--- resident.php
+++ resident.php
@@ -10,23 +10,22 @@ $method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
     case "GET":
 
-
-        if (isset($_GET['product_id'])) {
-            $product_id_spe = $_GET['product_id'];
-            $sql = "SELECT * FROM product INNER JOIN supplier ON supplier.supplier_id = product.supplier_id WHERE product_id = :product_id";
+        if (isset($_GET['resident_id'])) {
+            $resident_id = $_GET['resident_id'];
+            $sql = "SELECT * FROM resident WHERE resident_id = :resident_id";
         }
 
-
-        if (!isset($_GET['product_id'])) {
-            $sql = " SELECT * FROM product INNER JOIN supplier ON supplier.supplier_id = product.supplier_id";
+        if (!isset($_GET['resident_id'])) {
+            $sql = " SELECT * FROM resident ORDER BY resident_id DESC";
         }
+
 
 
         if (isset($sql)) {
             $stmt = $conn->prepare($sql);
 
-            if (isset($product_id_spe)) {
-                $stmt->bindParam(':product_id', $product_id_spe);
+            if (isset($resident_id)) {
+                $stmt->bindParam(':resident_id', $resident_id);
             }
 
             $stmt->execute();
@@ -36,7 +35,6 @@ switch ($method) {
         }
 
 
-
         break;
 
 
@@ -44,44 +42,43 @@ switch ($method) {
 
 
     case "POST":
-        $product = json_decode(file_get_contents('php://input'));
-        $sql = "INSERT INTO product (supplier_id, product_image, product_name, expiration_date, description, stocks, racks) VALUES (:supplier_id, :product_image, :product_name, :expiration_date, :description, :stocks, :racks)";
+        $resident = json_decode(file_get_contents('php://input'));
+        $sql = "INSERT INTO resident (resident_firstname, resident_middlename, resident_lastname, resident_extension, resident_birthday, resident_place_of_birth, resident_nationality, resident_religion, resident_weight, resident_height, resident_father_name, resident_mother_name, resident_houseno, resident_gender, resident_image, resident_type, resident_civilstatus, resident_purok) VALUES (:resident_firstname, :resident_middlename, :resident_lastname, :resident_extension, :resident_birthday, :resident_place_of_birth, :resident_nationality, :resident_religion, :resident_weight, :resident_height, :resident_father_name, :resident_mother_name, :resident_houseno, :resident_gender, :resident_image, :resident_type, :resident_civilstatus, :resident_purok)";
+
         $stmt = $conn->prepare($sql);
 
-        $stmt->bindParam(':supplier_id', $product->supplier_id);
-        $stmt->bindParam(':product_image', $product->product_image);
-        $stmt->bindParam(':product_name', $product->product_name);
+        $stmt->bindParam(':resident_firstname', $resident->resident_firstname);
+        $stmt->bindParam(':resident_middlename', $resident->resident_middlename);
+        $stmt->bindParam(':resident_lastname', $resident->resident_lastname);
+        $stmt->bindParam(':resident_extension', $resident->resident_extension);
+        $stmt->bindParam(':resident_birthday', $resident->resident_birthday);
+        $stmt->bindParam(':resident_place_of_birth', $resident->resident_place_of_birth);
+        $stmt->bindParam(':resident_nationality', $resident->resident_nationality);
+        $stmt->bindParam(':resident_religion', $resident->resident_religion);
+        $stmt->bindParam(':resident_weight', $resident->resident_weight);
+        $stmt->bindParam(':resident_height', $resident->resident_height);
+        $stmt->bindParam(':resident_father_name', $resident->resident_father_name);
+        $stmt->bindParam(':resident_mother_name', $resident->resident_mother_name);
+        $stmt->bindParam(':resident_houseno', $resident->resident_houseno);
 
-        $stmt->bindParam(':expiration_date', $product->expiration_date);
-        $stmt->bindParam(':description', $product->description);
-        $stmt->bindParam(':stocks', $product->stocks);
-        $stmt->bindParam(':racks', $product->racks);
 
+        $stmt->bindParam(':resident_gender', $resident->resident_gender);
+        $stmt->bindParam(':resident_image', $resident->resident_image);
+        $stmt->bindParam(':resident_type', $resident->resident_type);
+        $stmt->bindParam(':resident_civilstatus', $resident->resident_civilstatus);
+        $stmt->bindParam(':resident_purok', $resident->resident_purok);
 
 
         if ($stmt->execute()) {
 
-            $last_id = $conn->lastInsertId();
-            $type = "Stock In";
-            $sql3 = "INSERT INTO stock_history (product_name, type, quantity, created_at, product_id) VALUES (:product_name, :type, :quantity, :created_at, :product_id)";
-            $stmt3 = $conn->prepare($sql3);
-            $created_at = date('Y-m-d');
-            $stmt3->bindParam(':product_name', $product->product_name);
-            $stmt3->bindParam(':type', $type);
-            $stmt3->bindParam(':quantity', $product->stocks);
-            $stmt3->bindParam(':created_at', $created_at);
-            $stmt3->bindParam(':product_id', $last_id);
-
-            $stmt3->execute();
-
             $response = [
                 "status" => "success",
-                "message" => "product successfully"
+                "message" => "resident successfully"
             ];
         } else {
             $response = [
                 "status" => "error",
-                "message" => "product failed"
+                "message" => "resident failed"
             ];
         }
 
@@ -89,29 +86,62 @@ switch ($method) {
         break;
 
     case "PUT":
-        $product = json_decode(file_get_contents('php://input'));
-        $sql = "UPDATE product SET product_name= :product_name, description = :description, expiration_date = :expiration_date, racks = :racks
-                    WHERE product_id = :product_id";
+        $resident = json_decode(file_get_contents('php://input'));
+        $sql = "UPDATE resident 
+        SET 
+            resident_firstname = :resident_firstname,
+            resident_middlename = :resident_middlename,
+            resident_lastname = :resident_lastname,
+            resident_extension = :resident_extension,
+            resident_birthday = :resident_birthday,
+            resident_place_of_birth = :resident_place_of_birth,
+            resident_nationality = :resident_nationality,
+            resident_religion = :resident_religion,
+            resident_weight = :resident_weight,
+            resident_height = :resident_height,
+            resident_father_name = :resident_father_name,
+            resident_mother_name = :resident_mother_name,
+            resident_houseno = :resident_houseno,
+            resident_gender = :resident_gender,
+            resident_image = :resident_image,
+            resident_type = :resident_type,
+            resident_civilstatus = :resident_civilstatus,
+            resident_purok = :resident_purok
+        WHERE resident_id = :resident_id";
 
         $stmt = $conn->prepare($sql);
-        $updated_at = date('Y-m-d');
-        $stmt->bindParam(':product_id', $product->product_id);
-        $stmt->bindParam(':product_name', $product->product_name);
-        $stmt->bindParam(':description', $product->description);
-        $stmt->bindParam(':expiration_date', $product->expiration_date);
-        $stmt->bindParam(':racks', $product->racks);
+        $stmt->bindParam(':resident_id', $resident->resident_id);
+        $stmt->bindParam(':resident_firstname', $resident->resident_firstname);
+        $stmt->bindParam(':resident_middlename', $resident->resident_middlename);
+        $stmt->bindParam(':resident_lastname', $resident->resident_lastname);
+        $stmt->bindParam(':resident_extension', $resident->resident_extension);
+        $stmt->bindParam(':resident_birthday', $resident->resident_birthday);
+        $stmt->bindParam(':resident_place_of_birth', $resident->resident_place_of_birth);
+        $stmt->bindParam(':resident_nationality', $resident->resident_nationality);
+        $stmt->bindParam(':resident_religion', $resident->resident_religion);
+        $stmt->bindParam(':resident_weight', $resident->resident_weight);
+        $stmt->bindParam(':resident_height', $resident->resident_height);
+        $stmt->bindParam(':resident_father_name', $resident->resident_father_name);
+        $stmt->bindParam(':resident_mother_name', $resident->resident_mother_name);
+        $stmt->bindParam(':resident_houseno', $resident->resident_houseno);
 
+
+        $stmt->bindParam(':resident_gender', $resident->resident_gender);
+        $stmt->bindParam(':resident_image', $resident->resident_image);
+        $stmt->bindParam(':resident_type', $resident->resident_type);
+        $stmt->bindParam(':resident_civilstatus', $resident->resident_civilstatus);
+        $stmt->bindParam(':resident_purok', $resident->resident_purok);
 
         if ($stmt->execute()) {
 
             $response = [
                 "status" => "success",
-                "message" => "product updated successfully"
+                "message" => "resident updated successfully"
             ];
         } else {
             $response = [
                 "status" => "error",
-                "message" => "product update failed"
+                "message" => "resident update failed"
             ];
         }
 
@@ -119,20 +149,20 @@ switch ($method) {
         break;
 
     case "DELETE":
-        $product = json_decode(file_get_contents('php://input'));
-        $sql = "DELETE FROM product WHERE product_id = :product_id";
+        $resident = json_decode(file_get_contents('php://input'));
+        $sql = "DELETE FROM resident WHERE resident_id = :resident_id";
         $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':product_id', $product->product_id);
+        $stmt->bindParam(':resident_id', $resident->resident_id);
 
         if ($stmt->execute()) {
             $response = [
                 "status" => "success",
-                "message" => "product deleted successfully"
+                "message" => "resident deleted successfully"
             ];
         } else {
             $response = [
                 "status" => "error",
-                "message" => "product delete failed"
+                "message" => "resident delete failed"
             ];
         }
 
